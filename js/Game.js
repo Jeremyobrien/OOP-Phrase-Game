206cb1b98f406fb70432606e4e6fcf38710065d8
@Jeremyobrien <jjob1391@gmail.com>
2021-08-10 17:25:21 -0400

    Created classes

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,67 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+class Game{
+    constructor(){
+        this.missed = 0;
+        this.phrase = [];
+        this.activePhrase = null;
+    }
+    startGame(){
+        document.querySelector('#overlay').style.display = 'none';
+        this.activePhrase = this.getRandomPhrase;
+        this.phrase.addPhraseToDisplay(this.activePhrase);
+    }
+    get getRandomPhrase(){
+        return this.phrase[Math.floor(Math.random() * this.phrase.length)];
+    }
+    handleInteraction(){
+        qwerty.addEventListener('click', (e)=>{
+            e.target.disabled = true;
+            if(e.target.value !== this.phrase.checkLetter){
+                e.target.classList.add('wrong');
+                removeLife();
+            } else {
+                e.target.classList.add('chosen');
+                this.phrase.showMatchedLetter();
+                if(this.checkForWin()){
+                    gameOver();
+                }
+            }
+        })
+    }
+    removeLife(){
+        const scoreBoard = document.querySelector('#scoreboard');
+        const lives = scoreBoard.children;
+        if(!this.phrase.checkLetter){
+            const lostHeart = lives.children.find( image => image.src = "images/liveHeart.png" );
+            lostHeart.setAttribute('src', 'images/lostHeart.png');
+            this.missed ++;
+            if(this.missed === 5){
+                gameOver();
+            }
+        }
+    }
+    checkForWin(){
+        this.activePhrase.forEach(letter =>{
+            if (!letter.classList.contains('chosen')){
+                return false;
+            } else {
+                return true;
+            }
+        })
+    }
+    gameOver(){
+       const overlay = document.querySelector('#overlay').style.display = 'block';
+       const message = document.querySelector('#game-over-message');
+        if(this.checkForWin){
+            message.textContent = 'Congratulations, you won!';
+            overlay.classList.remove('start');
+            overlay.classList.add('win');
+        } else {
+            this.textContent = 'Game over..Play again?';
+            overlay.classList.remove('start');
+            overlay.classList.add('lose');
+        }
+    }
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,29 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+    constructor(phrase){
+        this.phrase = phrase.toLowerCase();
+    }
+    
+    addPhraseToDisplay(){
+        const phraseSection =  document.querySelector("#phrase");
+        let space = document.createElement('li');
+        const phrase = this.phrase.split('');
+        phrase.map(letter => space = letter);
+        phraseSection.push(phrase);
+        console.log(phraseSection);
+    }
+    checkLetter(){
+        const qwerty =  document.querySelector('#qwerty');
+        qwerty.addEventListener('click', (e)=>{
+           const matches = this.phrase.filter(letter => letter === e.target.value ? true : false);
+           return matches;
+        })
+    }
+    showMatchedLetter(){
+        matches.forEach(match => match.style.display.hide = false); 
+    }
+    
+}
